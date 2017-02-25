00f6b557e3fc0cd164f3bebfc523b6386cb1496b
Yuri M. Clene <costavold145@outlook.com>
2017-02-25 08:39:00 -0300

    Update admfinder.sh

--- admfinder.sh
+++ admfinder.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #by vold
 clear
-wget https://gist.githubusercontent.com/YuriMV/da83ee70e436b3ba2bd5f8eeba118e51/raw/3bfa2a541fd2f05cfceabd5b17f1b894ecd193ab/painel.txt 2>/dev/null
-rm -rf painel.txt.1 2>/dev/null
+wget https://gist.githubusercontent.com/YuriMV/da83ee70e436b3ba2bd5f8eeba118e51/raw/3bfa2a541fd2f05cfceabd5b17f1b894ecd193ab/painel.txt 2>/dev/null; rm -rf painel.txt.1 2>/dev/null
 read -p "Digite o site: " site
 printf "\n"
   for i in `cat painel.txt`
@@ -10,9 +9,9 @@ printf "\n"
     curlvar=$(curl -s -o /dev/null -w "%{http_code}" $site/$i)
         if [ "$curlvar" = "301" ] || [ "$curlvar" = "302" ] || [ "$curlvar" = "201" ]
           then
-          echo -n "Encontrado : \033[04;34m$site/$i\n\033[0m"
+          printf "Encontrado : \e[04;34m$site/$i\n\e[0m"
           break
            else
-          echo -n "\033[05;31mNao encontrou!\n"
+          printf "\e[05;31mNao encontrou!\n\e[0m"
           fi
           done
